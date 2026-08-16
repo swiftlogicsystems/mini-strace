f70c040c3a6f0a9b5c61098a58e54252c5300cca
Ankur Rathore <rathore.ankur@gmail.com>
2026-08-16 16:49:17 +0530

    added structs to manage App state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, Event, KeyCode},
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+};
+use nix::{
+    sys::{
+        ptrace,
+        wait::{WaitStatus, waitpid},
+    },
+    unistd::Pid,
+};
+use ratatui::{prelude::*, widgets::*};
+use std::{error::Error, io, sync::mpsc, thread, time::Duration};
+
+#[derive(Debug, Clone)]
+enum SyscallState {
+    UserSpace,      // Running in User Mode
+    EnteringKernel, // Just executed 'syscall'
+    InService,      // Kernel is processing
+    ExitingKernel,  // Just before returning to User
+}
+
+struct AppState {
+    syscall_name: String,
+    rax: u64,
+    rdi: u64,
+    state: SyscallState,
+    history: Vec<String>,
 }
