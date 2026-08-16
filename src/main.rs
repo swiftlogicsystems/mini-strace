99878bff29991740e4db06f14b5b1962014bee7f
Ankur Rathore <rathore.ankur@gmail.com>
2026-08-16 17:48:00 +0530

    added run_tracer functionality

--- src/main.rs
+++ src/main.rs
@@ -28,3 +28,83 @@ struct AppState {
     state: SyscallState,
     history: Vec<String>,
 }
+
+fn main() -> Result<(), Box<dyn Error>> {
+    //1. Setup Channel for communication between tracer thread and UI thread
+    let (rx, tx) = mpsc::channel();
+
+    //2. Fork and start the tracer
+    match unsafe { fork() }? {
+        ForkResult::Parent { child } => {
+            thread::spawn(move || run_tracer(child, tx));
+        }
+        ForkResult::Child => {
+            run_target_process();
+            return Ok(());
+        }
+    }
+
+    //3. Setting up the UI
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout))?;
+
+    let mut app_state = AppState {
+        syscall_name: "NONE".to_string(),
+        rax: 0,
+        rdi: 0,
+        state: SyscallState::UserSpace,
+        history: Vec::new(),
+    };
+
+    // Main UI Loop
+    loop {}
+
+    Ok(())
+}
+
+fn run_tracer(child: Pid, tx: mpsc::Sender<AppState>) {
+    waitpid(child, None).unwrap(); // Wait for the child process to exit
+    let mut in_syscall = false;
+    let mut history = Vec::new();
+
+    loop {
+        // Wait for the next syscall event
+        ptrace::syscall(child, None).unwrap();
+
+        match waitpid(child, None).unwrap() {
+            WaitStatus::PtraceSyscall(_) => {
+                let regs = ptrace::getregs(child).unwrap();
+                if !in_syscall {
+                    in_syscall = true;
+                    let name = format!("Syscall: {}", regs.rax);
+                    history.push(format!("Entered: {}", name));
+                    let _ = tx.send(AppState {
+                        syscall_name: name,
+                        rax: regs.rax,
+                        rdi: regs.rdi,
+                        state: SyscallState::EnteringKernel,
+                        history: history.clone(),
+                    });
+                    // slow down UI to make it easier to read
+                    thread::sleep(Duration::from_millis(400));
+                } else {
+                    // Exit Point
+                    in_syscall = false;
+                    history.push(format!("Result: {}", regs.rax));
+                    let _ = tx.send(AppState {
+                        syscall_name: "RETURNING".into(),
+                        rax: regs.rax,
+                        rdi: regs.rdi,
+                        state: SyscallState::ExitingKernel,
+                        history: history.clone(),
+                    });
+                    thread::sleep(Duration::from_millis(400));
+                }
+            }
+            WaitStatus::Exited(_, _) => break,
+            _ => {}
+        }
+    }
+}
